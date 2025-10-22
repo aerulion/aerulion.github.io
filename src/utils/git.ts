c25dbf8f71417d825c6b84291823b9983edb14ba
aerulion <aerulion@gmail.com>
2025-10-22 03:11:31 +0200

    :recycle: chore: Move CSS to components

--- src/utils/git.ts
+++ src/utils/git.ts
@@ -1,4 +1,4 @@
 export const commitHash = import.meta.env.PUBLIC_COMMIT_HASH || 'dev';
 export const shortCommitHash = commitHash.substring(0, 7);
 export const repoUrl = 'https://github.com/aerulion/aerulion.github.io';
-export const commitUrl = `${repoUrl}/commit/${commitHash}`;
+export const commitUrl = commitHash == 'dev' ? '#' : `${repoUrl}/commit/${commitHash}`;
