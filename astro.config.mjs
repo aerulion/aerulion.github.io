3e98ce1cbe57151d83670de99578db933061fb75
aerulion <aerulion@gmail.com>
2026-04-02 00:11:09 +0200

    :sparkles: feat: Redesign (#5)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,13 @@
-import {defineConfig} from 'astro/config';
+import {defineConfig, fontProviders} from 'astro/config';
 
 export default defineConfig({
-    site: 'https://aerulion.net',
-    base: '/',
+    site: 'https://aerulion.net', base: '/', fonts: [{
+        name: 'Inter', cssVariable: '--font-family', provider: fontProviders.fontsource(),
+    }, {
+        name: 'Inter Tight', cssVariable: '--font-family-display', provider: fontProviders.fontsource(),
+    }, {
+        name: 'Audiowide', cssVariable: '--font-family-accent', provider: fontProviders.fontsource(),
+    }, {
+        name: 'IBM Plex Mono', cssVariable: '--font-family-mono', provider: fontProviders.fontsource(),
+    }]
 });
