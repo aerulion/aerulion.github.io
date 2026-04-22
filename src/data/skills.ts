84404e24592a4a1a34cf6fc8828c57943946802e
aerulion <aerulion@gmail.com>
2026-04-23 01:20:57 +0200

    :fire: feat: Remove `SkillItem` component and refactor skill-related styles & data

--- src/data/skills.ts
+++ src/data/skills.ts
@@ -1,11 +1,10 @@
 export interface Skill {
-    icon: string;
     name: string;
 }
 
 export const skills: Skill[] = [
-    {icon: "☕", name: "Java Development"},
-    {icon: "🗄️", name: "Database Design"},
-    {icon: "🎮", name: "Paper & Bukkit"},
-    {icon: "🇩🇪", name: "Based in Germany"}
+    {name: "Java Development"},
+    {name: "Database Design"},
+    {name: "Paper & Bukkit"},
+    {name: "Based in Germany"}
 ];
